760c595520e6bfc9b869b752cd42f7a80ff91bfd
Sefalo AF <sefaloaf@gmail.com>
2023-08-04 21:23:21 +0700

    portofolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      strokeWidth: {
+        5: "5px",
+        3: "3px",
+      },
+      boxShadow: {
+        solid: "0.2em 0.2em #000",
+        solidHover: "0.0em 0.0em #000",
+      },
+      colors: {
+        primary: "#282B38",
+        secondary: "#35394A",
+        ownOrange: "#FFC814",
+      },
+      // letterSpacing: {
+      //   widest: ".25em",
+      // },
+    },
+  },
+  plugins: [require("daisyui")],
+};
